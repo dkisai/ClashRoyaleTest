7870abae079664faf5e965684b86c2fd62c29972
dkisai <diego.kisai@live.com>
2022-01-25 18:08:34 -0600

    First version

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import json
+import requests
 
 
 # PlayerTags
@@ -13,18 +15,60 @@ def clearConsole():
         command = 'cls'
     os.system(command)
 
-    
+
 class ClashRoyale:
     
     def __init__(self, tag):
         self.tag = tag
-        self.my_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3' \
-                        'MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjE0NmNlNmQ1LWU4NWEtNDhhNC04MGQzLTk1ZTM0ODY4MjllYSIsI' \
-                        'mlhdCI6MTYzOTI3MzEwOCwic3ViIjoiZGV2ZWxvcGVyLzU4YTViNTllLTM3MGUtZmM0NC1mOGEyLWIwNjgyN2Q3NzhkYSIsInNjb3BlcyI6WyJyb3lh' \
-                        'bGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIxODkuMTY5LjExMi4' \
-                        '4MyIsIjE4Ny4xNDAuMjA2LjE5NSJdLCJ0eXBlIjoiY2xpZW50In1dfQ._faEftsh-F-P0QGCkUfpc_s653X1H9gPXjJ-RSW0ySTTOXlX9d1N7GqMGYw' \
-                        'tVUPe3D1ZVKe7JKw1gu0esNVHjg'
+        self.my_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6Ijc0YTcwMGExLTU3OWYtNGUxNS1iZmRiLTA5YmNkYjA0YjYyYyIsImlhdCI6MTY0MzE0OTg0Miwic3ViIjoiZGV2ZWxvcGVyLzU4YTViNTllLTM3MGUtZmM0NC1mOGEyLWIwNjgyN2Q3NzhkYSIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIxODcuMTQwLjEuMjI0Il0sInR5cGUiOiJjbGllbnQifV19.AXcf5yyTf89JV6wYOgUJhZ1NwZMZVXXn6WzwS2NrEZWjAT1DDMUFxCZTxaJEqKwlhdV2hyhekgPYfGpF8xTKDQ'
 
+
+    def getData(self, url):
+        head = {f'authorization': f'Bearer {self.my_token}'}
+        resp = requests.get(url=url, headers=head)
+        data = json.loads(resp.text)
+        return data
+
+
+    def getPlayerName(self):
+        """Obtiene el nombre del jugador mediante el tag"""
+        url = 'https://api.clashroyale.com/v1/players/' + '%23' + self.tag
+        data = self.getData(url)
+        return data['name']
+
+
+    def getFavoriteCard(self):
+        """Obtiene la carta favorita del jugador mediante el tag"""
+        url = 'https://api.clashroyale.com/v1/players/' + '%23' + self.tag
+        data = self.getData(url)
+        return data['currentFavouriteCard']['name']
+
+
+    def getWinsLosses(self):
+        """Obtiene la cantidad de wins/losses del jugador mediante el tag"""
+        url = 'https://api.clashroyale.com/v1/players/' + '%23' + self.tag
+        data = self.getData(url)
+        return f"Tienes {data['wins']} victorias y {data['losses']} derrotas al dia de hoy"
+
+
+    def getNextChest(self):
+        """Obtiene el siguiente cofre"""
+        url = 'https://api.clashroyale.com/v1/players/' + '%23' + self.tag + '/upcomingchests'
+        data = self.getData(url)
+        for data['items'] in data['items']:
+            return f"En tu siguente cofre te saldra {(data['items']['name'])}"
+        #     print(f"Dentro de {(data['items']['index'])} cofres te saldra {(data['items']['name'])}")
+
+    
+    def checkplayertag(self):
+        """Valida la etiqueta para continuar con los procesos"""
+        url = 'https://api.clashroyale.com/v1/players/' + '%23' + self.tag
+        head = {f'authorization': f'Bearer {self.my_token}'}
+        resp = requests.get(url=url, headers=head)
+        if resp.status_code == 200:
+            return True
+        else:
+            return False
 
 
 if __name__ == "__main__":
